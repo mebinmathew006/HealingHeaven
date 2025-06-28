c36ff34e3e07966a54eab8e3979476fb74b5a39e
Mebin Mathew <mebinmathew006@gmail.com>
2025-06-29 03:48:01 +0530

    vercel files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/',
   plugins: [tailwindcss()],
   server: {
     host: '0.0.0.0',       // 🔓 Allow access from Docker host
